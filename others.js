817d9accac119e485a52a04ba4fd3bca6b556793
Elia <emrahkrkmz1@gmail.com>
2017-05-08 23:34:14 +0300

    Added all lines of poems

--- others.js
+++ others.js
@@ -5,6 +5,10 @@ module.exports = {
 		array[1] = "Ar ettim sakladım uğraşlarımı";
 		array[2] = "Ah seni bilmiyor kimseler bilmiyor";
 		array[3] = "Anlatamıyorum";
+		array[4] = "Aşkım, sonsuzum, bu dünyada ne var ne yok senindir";
+		array[5] = "Ayağa kalkarsın, adına uygunsun ve haklısın ";
+		array[6] = "Alışırım seni yalnız düşlerde okşamaya;"
+		array[7] = "Ağlasam sesimi duyar mısınız";
 		return array[Math.floor(Math.random()*array.length)];
 	},
 	getArrayB : function(){
@@ -13,6 +17,12 @@ module.exports = {
 		array[1] = "Belki körsün kırılmışsın telaş içindesin ";
 		array[2] = "Bir şilep sızıyor ıssız gözlerinden";
 		array[3] = "Bir kadeh mey için meyhânelerde,";
+		array[4] = "Bir gece başlar, yarı siyah, yarı kırmızı ";
+		array[5] = "Bir ölüm nefes alırken bir dudakta ";
+		array[6] = "Biliyor musun az az yaşıyorsun içimde ";
+		array[7] = "Bitti o sevda kesildi çığlıkları martıların"
+		array[8] = "Bilmezdim şarkıların bu kadar güzel , kelimelerinse kifayetsiz olduğunu";
+		array[9] = "Bir acımasız kalbin sevdası ile alevlendim.";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayC : function(){
 		var array = [];
@@ -27,6 +37,8 @@ module.exports = {
 		array[1] = "Çiçek Pasajı'nda akşam üstleri";
 		array[2] = "Çıkar giderim bu kentten daha olmazsa";
 		array[3] = "Çoktur benim merhametim ";
+		array[4] = "Çınar yapraklarının arasında yeşil yeşil ";
+		array[5] = "Çehren bana uğrunda ölüm hazzı verince";
 		return array[Math.floor(Math.random()*array.length)];
 	}
 	,getArrayD : function(){
@@ -35,6 +47,7 @@ module.exports = {
 		array[1] = "Dönerim dönerim çıkmaz.";
 		array[2] = "Dağlara çarptım her esişimde.";
 		array[3] = "Dokunabilir misiniz , Göz yaşlarıma, ellerinizle?";
+		array[4] = "Diyelim yağmura tutuldun bir gün ";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayE : function(){
 		var array = [];
@@ -42,6 +55,7 @@ module.exports = {
 		array[1] = "En leylim gecede ölesim tutmuş,";
 		array[2] = "Epeyce yaklaşmışım, duyuyorum";
 		array[3] = "Evler, apartmanlar üstüne yıkılır";
+		array[4] = "Ey en güzel görüntüsü çiçeklere dökülen bir çavlanın ";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayF : function(){
 		var array = [];
@@ -56,6 +70,7 @@ module.exports = {
 		array[1] = "Gümbür gümbür bir yürek diledik kavgamızda...";
 		array[2] = "Gökyüzüne bakmayı, dostlara mektup yazmayı";
 		array[3] = "Gülebildiğin kadar mutlusun";
+		array[4] = "Gönlüm güvertede sereserpedir.";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayH : function(){
 		var array = [];
@@ -63,6 +78,10 @@ module.exports = {
 		array[1] = "Hatırına düşeceğim belki";
 		array[2] = "Hoşça kal gözümün nuru";
 		array[3] = "Hayın, karanlıktı gece,";
+		array[4] = "Hayatın kutlu olsun sevgilim ";
+		array[5] = "Halbuki korkulacak hiçbir şey yoktu ortalıkta";
+		array[6] = "Her yanı, ama her yanı çocuklar gibi gülümseyen";
+		array[7] = "Her şeyi beni anlayınca anlayacaksın";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayI : function(){
 		var array = [];
@@ -77,6 +96,8 @@ module.exports = {
 		array[1] = "İliklerine işleyecek bensizlik";
 		array[2] = "İçinde mevsimler değişir, aynalar kırılır";
 		array[3] = "İşte budur yaşamak,";
+		array[4] = "İkimiz birden sevinebiliriz göğe bakalım";
+		array[5] = "İtti kıyıyı adına deniz dediğimiz şey";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayJ : function(){
 		var array = [];
@@ -91,6 +112,11 @@ module.exports = {
 		array[1] = "Kendinden bir parça bir cisim bırak";
 		array[2] = "Kötü rüzgar saçlarını götürüyor";
 		array[3] = "Kan tarlası gelincik şafağında...";
+		array[4] = "Kısacık bir zaman olmalıydı elimde..";
+		array[5] = "Korkmuyorum artık solmaktan";
+		array[6] = "Kim görürdü o yolcuyu, yani kim farkederdi beni";
+		array[7] = "Korularda yoğun bir erguvan sisi";
+		array[8] = "Kirpiksiz gözleri böyle daha güzel ";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayL : function(){
 		var array = [];
@@ -98,6 +124,7 @@ module.exports = {
 		array[1] = "Lugatinde kaçmak olmayan yiğit";
 		array[2] = "Lûgat, bir isim ver bana halimden;";
 		array[3] = "Lodos ince ince süzülüyor sarmaşıklardan";
+		array[4] = "Lapinalar gümüşler açıyor gönülde ";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayM : function(){
 		var array = [];
@@ -119,6 +146,7 @@ module.exports = {
 		array[1] = "Onsuz can veririm ben bu cana";
 		array[2] = "Olsa da olmasa da, amaçsız çaban mıyım?";
 		array[3] = "Ortalığa düşmüşüm seni arıyorum.";
+		array[4] = "Oysaki seninle güzel olmak var ";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayOo : function(){
 		var array = [];
@@ -133,6 +161,10 @@ module.exports = {
 		array[1] = "Papatya bahçesi kokusunu çekercesine";
 		array[2] = "Paydos bundan böyle çılgınlıklara!";
 		array[3] = "Pencereleri açmalı, kitapları düzenlemeliyim ";
+		array[4] = "Pır pır eder yüreğim bakındıkça... ";
+		array[5] = "Pırıl pırıl dalgalı bir denize karşı ";
+		array[6] = "Parmakları capcanlı, pavuryalar gibi ";
+		array[7] = "Pırıl pırıl düşüyor damlalar ";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayR : function(){
 		var array = [];
@@ -147,6 +179,8 @@ module.exports = {
 		array[1] = "Sevinçten uçardım hasta oldum mu";
 		array[2] = "Sonra sıcak yatağında uzun uzun";
 		array[3] = "Sen hem bir hastalık hem de sağlık gibisin";
+		array[4] = "Sevgilim sevgilim ";
+		array[5] = "Senindir ey sonsuzveren ne varsa hayat gibi ";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArraySs : function(){
 		var array = [];
@@ -154,6 +188,7 @@ module.exports = {
 		array[1] = "Şiir, düş, umut ve hülyâ";
 		array[2] = "Şiirin en güzel sabahı,";
 		array[3] = "Şu dilime tutturduğum bu sevda türküsü olmasaydı";
+		array[4] = "Şu aranıp duran korkak ellerimi tut";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayT : function(){
 		var array = [];
@@ -168,6 +203,7 @@ module.exports = {
 		array[1] = "Uzun bir hastalık gibi ";
 		array[2] = "Unutma yagmurun yağdığı kadar ıslaksın, ";
 		array[3] = "Ufuksuz bir ummandan alıp getirdikleriyle ";
+		array[4] = "Uykudan uyandırsam seni..";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayUu : function(){
 		var array = [];
@@ -189,6 +225,8 @@ module.exports = {
 		array[1] = "Yürümeyi yeniden öğrenen felçli bir çocuk gibi ";
 		array[2] = "Yarıda bıraktığım şiirleri tamamlarım ";
 		array[3] = "Yaşadığın kadar yakınsın sonuna; ne kadar yaşarsan yaşa,";
+		array[4] = "Yürüyorum kumların çakılların yanı sıra ";
+		array[5] = "Yürüyorum, arkama bakmadan yürüyorum.";
 		return array[Math.floor(Math.random()*array.length)];
 	},getArrayZ : function(){
 		var array = [];
